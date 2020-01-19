241d1fc286f9df75134bd7edef9223e095e1250c
jagjotsk1999 <jagjotsk1999@gmail.com>
2020-01-19 22:22:01 +0530

    jan 19

--- StringReverseWords.java
+++ StringReverseWords.java
@@ -6,19 +6,10 @@ public class StringReverseWords {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s=sc.next();
-        String output="", temp="";
-        for (int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if (c=='.' || i==s.length()-1){
-                if (i!=s.length()-1){
-                    temp=c+temp;
-                }
-                else temp=temp+c;
-                output=temp+output;
-                temp="";
-            }
-            else temp=temp+c;
+        String[] array=s.split("\\.");
+        for (int i=array.length-1;i>0;i--){
+            System.out.print(array[i]+".");
         }
-        System.out.println(output);
+        System.out.println(array[0]);
     }
 }
